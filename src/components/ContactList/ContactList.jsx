75e8b9d9756c1a9563098b119090cbca6d2be4e9
Myroslav Sokolov <mirossoks@gmail.com>
2022-05-02 23:13:25 +0300

    1

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,19 +1,27 @@
 import { getFilter, getContacts } from 'redux/contactsSlice';
 import { useSelector } from 'react-redux';
+import * as React from 'react'
+import { useGetContactsQuery } from '../../services/contacts'
 import ContactListItem from '../ContactListItem';
 
 export default function ContactList() {
   const filter = useSelector(getFilter);
   const contacts = useSelector(getContacts);
+  const { data, error, isLoading } = useGetContactsQuery()
 
   const filterContacts = contacts.filter(contact =>
     contact.name.toLowerCase().includes(filter.toLowerCase())
   );
+  console.log(data)
   return (
     <ul>
-      {filterContacts.map(({ id, name, number }) => (
-        <ContactListItem key={id} id={id} name={name} number={number} />
-      ))}
+      {error ? (
+        <>Oh no, there was an error</>
+      ) : isLoading ? (
+        <>Loading...</>
+      ) : data ? (data.map(({ id, name, phone }) => (
+        <ContactListItem key={id} id={id} name={name} number={phone}  />
+      ))) : null}
     </ul>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,24 +1,64 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { persistStore,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER, } from 'redux-persist';
-import { contactsReduser } from './contactsSlice';
+import { configureStore } from '@reduxjs/toolkit'
+// Or from '@reduxjs/toolkit/query/react'
+import { setupListeners } from '@reduxjs/toolkit/query'
+import { contactsApi } from '../services/contacts'
 
 export const store = configureStore({
   reducer: {
-    contacts: contactsReduser,
+    // Add the generated reducer as a specific top-level slice
+    [contactsApi.reducerPath]: contactsApi.reducer,
   },
-      middleware(getDefaultMiddleware) {
-      return getDefaultMiddleware({
-        serializableCheck: {
-          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-        },
-      });
-  },
-});
+  // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(contactsApi.middleware),
+})
 
-export const persistor = persistStore(store);
+// optional, but required for refetchOnFocus/refetchOnReconnect behaviors
+// see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
+setupListeners(store.dispatch)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { configureStore } from '@reduxjs/toolkit';
+// import { persistStore,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER, } from 'redux-persist';
+// import { contactsReduser } from './contactsSlice';
+
+// export const store = configureStore({
+//   reducer: {
+//     contacts: contactsReduser,
+//   },
+//       middleware(getDefaultMiddleware) {
+//       return getDefaultMiddleware({
+//         serializableCheck: {
+//           ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//         },
+//       });
+//   },
+// });
+
+// export const persistor = persistStore(store);
