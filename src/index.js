9a6ed9bdec7f0aaccf9b426720354a02b8a59e8b
Myroslav Sokolov <85230987+MiorsSoks@users.noreply.github.com>
2022-05-08 20:13:32 +0300

    bagfix persist

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react'
 import './index.css';
 import App from './App';
-import { store, persistor } from './redux/store';
+import { store } from './redux/store';
 
 
 const rootElement = document.getElementById('root');
